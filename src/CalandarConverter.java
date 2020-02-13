6188a772b4f50c67ceebf9b1ba3c5ccf6d4b513b
Sebastian.Blazejewski <blazejewski.s.m@gmail.com>
2020-02-13 10:55:24 +0100

    Final commit

---
+++ src/CalandarConverter.java
@@ -0,0 +1,23 @@
+class CalandarConverter {
+
+    static final int MONDAY = 1;
+    static final int TUESDAY = 2;
+    static final int WEDNESDAY = 3;
+    static final int THURSDAY = 4;
+    static final int FRIDAY = 5;
+    static final int SATURDAY = 6;
+    static final int SUNDAY = 7;
+
+    static String convertDayToString(int dayNumber){
+        return switch(dayNumber){
+            case 1 -> "Poniedziałek";
+            case 2 -> "Wtorek";
+            case 3 -> "Środa";
+            case 4 -> "Czwartek";
+            case 5 -> "Piątek";
+            case 6 -> "Sobota";
+            case 7 -> "Niedziela";
+            default -> "";
+        };
+    }
+}
